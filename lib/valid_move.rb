a1e47e99b0795874858f3c1f07d32ba43208b742
victoriaehrbar <victoriaehrbar@gmail.com>
2020-10-28 02:05:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,8 @@
-def valid_move?(board, index)
-  #if the position is not taken 
-end
-
-
 def position_taken?(board, index)
 
 board[index] != " " && board[index] != nil && board[index] != ""
+end
+
+def valid_move?(board, index)
+  #if the position is not taken 
 end
